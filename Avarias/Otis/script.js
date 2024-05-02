37781011bde04a65e3738c03b199980943126c2f
Mmarciomiguel <marciorodrigo2@hotmail.com>
2024-05-02 20:35:10 +0100

    barra de pesquisas

--- Avarias/Otis/script.js
+++ Avarias/Otis/script.js
@@ -16,3 +16,18 @@ document.querySelectorAll('.button').forEach(button => {
     button.classList.toggle('clicked'); // Alternar entre adicionar e remover a classe "clicked"
   });
 });
+document.getElementById('searchBar').addEventListener('input', function() {
+  var searchTerm = this.value.toLowerCase(); // Obtém o valor da barra de pesquisa em minúsculas
+
+  // Percorre todos os botões
+  document.querySelectorAll('.button').forEach(button => {
+    var buttonText = button.querySelector('span').textContent.toLowerCase(); // Obtém o texto dentro do span em minúsculas
+
+    // Verifica se o texto do botão contém o termo de pesquisa
+    if (buttonText.includes(searchTerm)) {
+      button.style.display = 'inline-block'; // Mostra o botão se corresponder ao termo de pesquisa
+    } else {
+      button.style.display = 'none'; // Esconde o botão se não corresponder ao termo de pesquisa
+    }
+  });
+});
